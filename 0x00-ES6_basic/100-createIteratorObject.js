26afeb36a8cc6a5b5a0461836f821b5b16365f24
FREDRICK KYEKI <fredrickisaac142@gmail>
2023-11-27 21:02:44 +0300

    ES6-basics

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,20 @@
+export default function createIteratorObject(report) {
+    return {
+        *[Symbol.iterator]() {
+            const departments = Object.values(report.allEmployees);
+            let employeeIndex = 0;
+            let departmentIndex = 0;
+            while (departmentIndex < departments.length) {
+                const employees = departments[departmentIndex];
+                if (employeeIndex >= employees.length) {
+                    employeeIndex = 0;
+                    departmentIndex += 1;
+                } else {
+                    const pair = employees[employeeIndex];
+                    employeeIndex += 1;
+                    yield pair;
+                }
+            }
+        }
+    };
+}
